d94c6bba251c2abff2e3f73bfe6d89220a98ab13
NNVI <thi.nguyenvan@elca.vn>
2026-07-14 14:17:14 +0700

    Adapt message bundle translate

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,17 +1,20 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader, provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 import { errorInterceptor } from './services/error-interceptor';
+import { PaginatorI18nService } from './services/paginator-i18n.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor])),
+    { provide: MatPaginatorIntl, useClass: PaginatorI18nService },
     provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
     provideTranslateService({ loader: TranslateHttpLoader, lang: 'en' }),
   ],
---
+++ client/src/app/services/paginator-i18n.service.ts
@@ -0,0 +1,30 @@
+import {inject, Injectable} from '@angular/core';
+import {MatPaginatorIntl} from '@angular/material/paginator';
+import {TranslateService} from '@ngx-translate/core';
+
+@Injectable()
+export class PaginatorI18nService extends MatPaginatorIntl {
+  private readonly translate = inject(TranslateService);
+
+  constructor() {
+    super();
+    this.translate.onLangChange.subscribe(() => this.updateLabels());
+    this.updateLabels();
+  }
+
+  private updateLabels() {
+    this.itemsPerPageLabel = this.translate.instant('pagination.itemsPerPage');
+    this.nextPageLabel = this.translate.instant('pagination.nextPage');
+    this.previousPageLabel = this.translate.instant('pagination.previousPage');
+    this.firstPageLabel = this.translate.instant('pagination.firstPage');
+    this.lastPageLabel = this.translate.instant('pagination.lastPage');
+    this.changes.next();
+  }
+
+  override getRangeLabel = (page: number, pageSize: number, length: number): string => {
+    if (length === 0) { return this.translate.instant('pagination.empty'); }
+    const start = page * pageSize + 1;
+    const end = Math.min((page + 1) * pageSize, length);
+    return this.translate.instant('pagination.showing', { start, end, total: length });
+  };
+}
